5c957419703835ef5aff0a7b0a58e4180b5fc985
Kylie Brown <kbrown1@ucvts.tec.nj.us>
2018-09-18 14:51:51 -0400

    Second commit

--- src/InputOutput.java
+++ src/InputOutput.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 
 public class InputOutput {
 	public static void main (String[] args) {
+		System.out.println("What's your name?");
 		Scanner in = new Scanner(System.in);
 		String text = in.nextLine();
-		System.out.println(text);
+		System.out.println("Hello, " + text + "!");
 		in.close();
 	}
 }
